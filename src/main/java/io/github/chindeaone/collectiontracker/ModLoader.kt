85f02779f41c9d0ddaa6bfbacc198a83bc167f12
Chindea Eduard <eduard.chindea1@gmail.com>
2025-12-30 14:47:17 +0200

    Added event registration and screens

--- src/main/java/io/github/chindeaone/collectiontracker/ModLoader.kt
+++ src/main/java/io/github/chindeaone/collectiontracker/ModLoader.kt
@@ -4,16 +4,41 @@
 package io.github.chindeaone.collectiontracker
 
 import io.github.chindeaone.collectiontracker.SkyblockCollectionTracker.modules
+import io.github.chindeaone.collectiontracker.commands.CommandRegistry
+import io.github.chindeaone.collectiontracker.util.Hypixel
+import io.github.chindeaone.collectiontracker.util.ServerUtils
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
+import net.minecraft.client.Minecraft
 
 class ModLoader: ModInitializer {
 
     override fun onInitialize() {
-        ClientTickEvents.END_CLIENT_TICK.register {it.tick()}
+        eventRegistration()
+
         SkyblockCollectionTracker.preInit()
         SkyblockCollectionTracker.init()
         loadedClasses.clear()
+
+        CommandRegistry.init()
+    }
+
+    private fun eventRegistration() {
+        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick {client ->
+            if (client.player == null) return@EndTick
+            TickDispatcher.onEndClientTick(client)
+        })
+        ClientPlayConnectionEvents.DISCONNECT.register{ _, _ -> Hypixel.onDisconnect() }
+    }
+
+    private object TickDispatcher {
+        fun onEndClientTick(client: Minecraft) {
+            // Call every onTick here
+            SkyblockCollectionTracker.onTick(client)
+            ServerUtils.onClientTick(client)
+            Hypixel.onTick(client)
+        }
     }
 
     companion object {
--- src/main/java/io/github/chindeaone/collectiontracker/SkyblockCollectionTracker.kt
+++ src/main/java/io/github/chindeaone/collectiontracker/SkyblockCollectionTracker.kt
@@ -2,9 +2,10 @@ package io.github.chindeaone.collectiontracker
 
 import io.github.chindeaone.collectiontracker.config.ConfigManager
 import io.github.chindeaone.collectiontracker.util.ModulesLoader
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.SharedConstants
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.Screen
-import java.util.ArrayList
 import java.util.logging.Logger
 
 object SkyblockCollectionTracker {
@@ -24,30 +25,28 @@ object SkyblockCollectionTracker {
             Thread { configManager.save() },
         )
     }
-
-    fun onTick() {
+    fun onTick(client: Minecraft) {
         val screenToOpen = screenToOpen ?: return
         screenTicks++
         if (screenTicks != 5) return
+        shouldCloseScreen = true
         Minecraft.getInstance().setScreen(screenToOpen)
         screenTicks = 0
         this.screenToOpen = null
     }
 
     var screenToOpen: Screen? = null
+    var shouldCloseScreen: Boolean = true
     private var screenTicks = 0
 
     val modules: MutableList<Any> = ArrayList()
     lateinit var configManager: ConfigManager
 
-    const val VERSION: String = "1.0.8"
+    const val NAMESPACE: String = "sct"
+    const val MODID = "skyblockcollectiontracker"
 
-
-
-//	override fun onInitialize() {
-//		// This code runs as soon as Minecraft is in a mod-load-ready state.
-//		// However, some things (like resources) may still be uninitialized.
-//		// Proceed with mild caution.
-//		logger.info("Hello Fabric world!")
-//	}
+    @JvmField
+    val VERSION = FabricLoader.getInstance().getModContainer(MODID).orElseThrow().metadata.version.friendlyString
+    @JvmField
+    val MC_VERSION: String = SharedConstants.getCurrentVersion().name()
 }
