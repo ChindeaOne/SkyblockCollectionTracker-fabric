c135336afe0041255fd698ae963316cb7ce83ad8
Chindea Eduard <eduard.chindea1@gmail.com>
2026-01-04 22:36:15 +0200

    Added a 1s buffer to allow the lobby data to load

--- src/main/java/io/github/chindeaone/collectiontracker/util/tab/CommissionsWidget.kt
+++ src/main/java/io/github/chindeaone/collectiontracker/util/tab/CommissionsWidget.kt
@@ -9,19 +9,34 @@ object CommissionsWidget {
     var rawCommissions: List<String> = emptyList()
 
     private var nextAllowedTime: Long = 0L
+    private var firstInfoSeenTime: Long = 0L
 
     fun onTabWidgetsUpdate() {
         val widget = TabWidget.COMMISSIONS
+        val now = System.currentTimeMillis()
+
         if (!widget.isPresent) {
-            if (!TabWidget.INFO.isPresent) return // avoid spamming messages when tab widgets are not visible
+            // avoid spamming messages when tab widgets are not visible
+            if (!TabWidget.INFO.isPresent) {
+                firstInfoSeenTime = 0L
+                return
+            }
+
+            if (firstInfoSeenTime == 0L) {
+                firstInfoSeenTime = now
+            }
+
+            if (now - firstInfoSeenTime < 1_000L) {
+                return // Wait for the 1-second buffer
+            }
 
             // disable the overlay if the widget is not found
             ChatUtils.sendMessage("§cCommissions widget not found. Please enable it using /widget.", true)
             SkyblockCollectionTracker.configManager.config!!.mining.commissionsOverlay.enableCommissionsOverlay = false
             return
         }
+        firstInfoSeenTime = 0L
 
-        val now = System.currentTimeMillis()
         if (now < nextAllowedTime) return
 
         val currentRaw = parseCommissionWidget(widget.lines)
