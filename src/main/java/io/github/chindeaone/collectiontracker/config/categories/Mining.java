9d49e490fd028d7f791da9d7b7841a1684531696
Chindea Eduard <eduard.chindea1@gmail.com>
2026-01-28 21:56:07 +0200

    Moved Commissions Keybinds config from Mining to CommissionsOverlay

--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/Mining.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/Mining.java
@@ -1,18 +1,12 @@
 package io.github.chindeaone.collectiontracker.config.categories;
 
 import com.google.gson.annotations.Expose;
-import io.github.chindeaone.collectiontracker.config.categories.mining.KeybindConfig;
 import io.github.chindeaone.collectiontracker.config.categories.overlay.CommissionsOverlay;
 import io.github.chindeaone.collectiontracker.config.categories.overlay.MiningStatsOverlay;
 import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class Mining {
-
-    @Expose
-    @ConfigOption(name = "Commissions Keybinds", desc = "")
-    @Accordion
-    public KeybindConfig commissions = new KeybindConfig();
 
     @Expose
     @ConfigOption(name = "Commissions Overlay", desc = "")
--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/CommissionsOverlay.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/CommissionsOverlay.java
@@ -1,7 +1,9 @@
 package io.github.chindeaone.collectiontracker.config.categories.overlay;
 
 import com.google.gson.annotations.Expose;
+import io.github.chindeaone.collectiontracker.config.categories.mining.KeybindConfig;
 import io.github.chindeaone.collectiontracker.config.core.Position;
+import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
@@ -18,4 +20,9 @@ public class CommissionsOverlay {
     @Expose
     @ConfigLink(owner = CommissionsOverlay.class, field = "commissionsOverlay")
     public Position commissionsOverlayPosition = new Position(50, 50);
+
+    @Expose
+    @ConfigOption(name = "Commissions Keybinds", desc = "")
+    @Accordion
+    public KeybindConfig commissions = new KeybindConfig();
 }
