5163544ec2a635f86e39e6a4de770c48600ad170
Chindea Eduard <eduard.chindea1@gmail.com>
2026-02-04 20:58:13 +0200

    Refactored overlay names

--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/Tracking.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/Tracking.java
@@ -2,54 +2,32 @@ package io.github.chindeaone.collectiontracker.config.categories;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import io.github.chindeaone.collectiontracker.config.categories.overlay.SingleOverlay;
+import io.github.chindeaone.collectiontracker.config.categories.overlay.CollectionOverlay;
+import io.github.chindeaone.collectiontracker.config.categories.overlay.SkillOverlay;
 import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class Tracking {
 
     @Expose
-    @ConfigOption(name = "Single Overlay", desc = "")
-    @SerializedName("single_overlay")
+    @ConfigOption(name = "Collection Overlay", desc = "")
+    @SerializedName("collection_overlay")
     @Accordion
-    public SingleOverlay singleOverlay = new SingleOverlay();
+    public CollectionOverlay collectionOverlay = new CollectionOverlay();
+
+    @Expose
+    @ConfigOption(name = "Skills Tracking Overlay", desc = "")
+    @SerializedName("skillOverlay")
+    @Accordion
+    public SkillOverlay skillOverlay = new SkillOverlay();
 
     @Expose
     @ConfigOption(
-            name = "Overlay Text Color",
-            desc = "Toggle this to enable color-coded overlay."
+            name = "Explicit values",
+            desc = "Show full values instead of rounded values for the overlays and summary."
     )
-    @SerializedName("overlayTextColor")
+    @SerializedName("explicitValues")
     @ConfigEditorBoolean
-    public boolean overlayTextColor = false;
-
-    @Expose
-    @ConfigOption(
-            name = "Tracking Summary",
-            desc = "Show tracking rates summary in chat when stopping a tracking session."
-    )
-    @SerializedName("showTrackingRatesAtEndOfSession")
-    @ConfigEditorBoolean
-    public boolean showTrackingRatesAtEndOfSession = true;
-
-    @Expose
-    @ConfigOption(
-            name = " §b[WIP]§r Sacks Tracking",
-            desc = "Toggle this to use Hypixel's sacks messages instead of api calls when tracking.\n§eRecommended to use only if you have a lot of sacks!"
-    )
-    @SerializedName("enableSacksTracking")
-    @ConfigEditorBoolean
-    public boolean enableSacksTracking = false;
-
-    @Expose
-    @ConfigOption(
-            name = "More Sacks Tracking Info",
-            desc = "§eFor some collections, it can be more accurate then API calls. " +
-                    "However, for now, API calls will still be used, and every API call will update the rates according to the API data, meaning rates can jump around."
-    )
-    @ConfigEditorInfoText
-    @SuppressWarnings("unused")
-    public boolean sacksInfo = true;
+    public boolean explicitValues = false;
 }
--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/CollectionOverlay.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/CollectionOverlay.java
@@ -2,17 +2,14 @@ package io.github.chindeaone.collectiontracker.config.categories.overlay;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
+import io.github.notenoughupdates.moulconfig.annotations.*;
 import io.github.chindeaone.collectiontracker.config.core.Position;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SingleOverlay {
+public class CollectionOverlay {
 
     public enum OverlayText {
         COLLECTION("§aGold collection§f: 200.000M"),
@@ -83,14 +80,42 @@ public class SingleOverlay {
 
     @Expose
     @ConfigOption(
-            name = "Explicit values",
-            desc = "Show full values instead of rounded values in the overlay and summary."
+            name = "Overlay Text Color",
+            desc = "Toggle this to enable color-coded overlay."
     )
-    @SerializedName("explicitValues")
+    @SerializedName("overlayTextColor")
     @ConfigEditorBoolean
-    public boolean explicitValues = false;
+    public boolean overlayTextColor = false;
 
     @Expose
-    @ConfigLink(owner = SingleOverlay.class, field = "singleStatsOverlay")
+    @ConfigOption(
+            name = "Tracking Summary",
+            desc = "Show tracking rates summary in chat when stopping a tracking session."
+    )
+    @SerializedName("showTrackingRatesAtEndOfSession")
+    @ConfigEditorBoolean
+    public boolean showTrackingRatesAtEndOfSession = true;
+
+    @Expose
+    @ConfigOption(
+            name = " §b[WIP]§r Sacks Tracking",
+            desc = "Toggle this to use Hypixel's sacks messages instead of api calls when tracking.\n§eRecommended to use only if you have a lot of sacks!"
+    )
+    @SerializedName("enableSacksTracking")
+    @ConfigEditorBoolean
+    public boolean enableSacksTracking = false;
+
+    @Expose
+    @ConfigOption(
+            name = "More Sacks Tracking Info",
+            desc = "§eFor some collections, it can be more accurate then API calls. " +
+                    "However, for now, API calls will still be used, and every API call will update the rates according to the API data, meaning rates can jump around."
+    )
+    @ConfigEditorInfoText
+    @SuppressWarnings("unused")
+    public boolean sacksInfo = true;
+
+    @Expose
+    @ConfigLink(owner = CollectionOverlay.class, field = "singleStatsOverlay")
     public Position overlayPosition = new Position(50, 100);
 }
