528843441aefb66615a06c7e382ea9b08ff16823
Chindea Eduard <eduard.chindea1@gmail.com>
2026-02-08 13:01:08 +0200

    Integrated professional/strong arm HOTM perks into mining speed parsing; added toggleable detailed mining fortune

--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/MiningStatsOverlay.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/MiningStatsOverlay.java
@@ -2,9 +2,8 @@ package io.github.chindeaone.collectiontracker.config.categories.overlay;
 
 import com.google.gson.annotations.Expose;
 import io.github.chindeaone.collectiontracker.config.core.Position;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import io.github.notenoughupdates.moulconfig.annotations.*;
+import io.github.notenoughupdates.moulconfig.observer.Property;
 
 public class MiningStatsOverlay {
 
@@ -18,10 +17,34 @@ public class MiningStatsOverlay {
     @Expose
     @ConfigOption(
             name = "Only in mining specific areas",
-            desc = "If enabled, the mining stats overlay will only show when you are in any Mining Islands."
+            desc = "Allows the overlay to be rendered only in Mining Islands."
     )
     @ConfigEditorBoolean
     public boolean miningStatsOverlayInMiningIslandsOnly = false;
+
+    @Expose
+    @ConfigOption(
+            name = "Show detailed fortune",
+            desc = "Shows the fortune breakdown in the overlay."
+    )
+    @ConfigEditorBoolean
+    public boolean showDetailedFortune = false;
+
+    @Expose
+    @ConfigOption(
+            name = "Professional mining speed",
+            desc = "Input your mining speed from Professional perk."
+    )
+    @ConfigEditorSlider(minValue = 0, maxValue = 755, minStep = 1)
+    public Property<Integer> professionalMS = Property.of(0);
+
+    @Expose
+    @ConfigOption(
+            name = "Strong Arm mining speed",
+            desc = "Input your mining speed from Strong Arm perk."
+    )
+    @ConfigEditorSlider(minValue = 0, maxValue = 505, minStep = 1)
+    public Property<Integer> strongArmMS = Property.of(0);
 
     @Expose
     @ConfigLink(owner = MiningStatsOverlay.class, field = "miningStatsOverlay")
