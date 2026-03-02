1d89f7fb94e0b3e5c77858cbeb5cc8018623cd3d
Chindea Eduard <eduard.chindea1@gmail.com>
2026-03-02 21:06:35 +0200

    Added tracking options configuration for multi tracking

--- src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/MultiCollectionOverlay.java
+++ src/main/java/io/github/chindeaone/collectiontracker/config/categories/overlay/MultiCollectionOverlay.java
@@ -2,11 +2,40 @@ package io.github.chindeaone.collectiontracker.config.categories.overlay;
 
 import com.google.gson.annotations.Expose;
 import io.github.chindeaone.collectiontracker.config.core.Position;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class MultiCollectionOverlay {
 
+    public enum TrackingOptions {
+        COLLECTION("Collection"),
+        COLLECTION_RATE("Collection Rate"),
+        COLLECTION_MADE("Collection Made"),
+        MONEY_RATE("Money Rate"),
+        MONEY_MADE("Money Made");
+
+        private final String displayName;
+
+        TrackingOptions(String displayName) {
+            this.displayName = displayName;
+        }
+
+        @Override
+        public String toString() {
+            return displayName;
+        }
+    }
+
+    @Expose
+    @ConfigOption(
+            name = "Tracking Selection",
+            desc = "Select what shows for each collection."
+    )
+    @ConfigEditorDropdown
+    public TrackingOptions trackingOptions = TrackingOptions.COLLECTION; // Default to collection amount
+
     @Expose
     @ConfigLink(owner = MultiCollectionOverlay.class, field = "multiCollectionOverlay")
-    public Position multiOverlayPosition = new Position(50,500);
+    public Position multiOverlayPosition = new Position(50,100);
 }
