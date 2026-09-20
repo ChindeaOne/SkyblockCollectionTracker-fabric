aaad10cf88503fa98266966c9577972d6169aa7e
Chindea Eduard <eduard.chindea1@gmail.com>
2026-09-20 23:17:40 +0300

    Simplified when statements

--- src/main/java/io/github/chindeaone/collectiontracker/utils/ColorUtils.kt
+++ src/main/java/io/github/chindeaone/collectiontracker/utils/ColorUtils.kt
@@ -150,16 +150,16 @@ object ColorUtils {
         }
 
         return when (rank) {
-            1 -> Color(0, 191, 255).toChromaColor()
-            2 -> Color(255, 215, 0).toChromaColor()
-            3 -> Color(192, 192, 192).toChromaColor()
-            in 4..25 -> Color(70, 130, 180).toChromaColor()
-            in 26..100 -> Color(0, 255, 255).toChromaColor()
-            in 101..250 -> Color(176, 196, 222).toChromaColor()
-            in 251..500 -> Color(47, 79, 79).toChromaColor()
-            in 501..1000 -> Color(112, 128, 144).toChromaColor()
-            else -> Color.WHITE.toChromaColor()
-        }
+            1 -> Color(0, 191, 255)
+            2 -> Color(255, 215, 0)
+            3 -> Color(192, 192, 192)
+            in 4..25 -> Color(70, 130, 180)
+            in 26..100 -> Color(0, 255, 255)
+            in 101..250 -> Color(176, 196, 222)
+            in 251..500 -> Color(47, 79, 79)
+            in 501..1000 -> Color(112, 128, 144)
+            else -> Color.WHITE
+        }.toChromaColor()
     }
 
     fun getFWRankColor(rank: Int, isMe: Boolean, playerName: String): ChromaColour {
@@ -179,16 +179,16 @@ object ColorUtils {
         }
 
         return when (rank) {
-            1 -> Color(255, 215, 0).toChromaColor()
-            2 -> Color(255, 140, 0).toChromaColor()
-            3 -> Color(139, 69, 19).toChromaColor()
-            in 4..25 -> Color(34, 139, 34).toChromaColor()
-            in 26..100 -> Color(173, 255, 47).toChromaColor()
-            in 101..250 -> Color(218, 165, 32).toChromaColor()
-            in 251..500 -> Color(244, 164, 96).toChromaColor()
-            in 501..1000 -> Color(107, 142, 35).toChromaColor()
-            else -> Color.WHITE.toChromaColor()
-        }
+            1 -> Color(255, 215, 0)
+            2 -> Color(255, 140, 0)
+            3 -> Color(139, 69, 19)
+            in 4..25 -> Color(34, 139, 34)
+            in 26..100 -> Color(173, 255, 47)
+            in 101..250 -> Color(218, 165, 32)
+            in 251..500 -> Color(244, 164, 96)
+            in 501..1000 -> Color(107, 142, 35)
+            else -> Color.WHITE
+        }.toChromaColor()
     }
 
     fun collToColor(collection: String): Component = Component.literal(collection).withColor(collectionColors[collection.lowercase()] ?: Colors.WHITE.color)
